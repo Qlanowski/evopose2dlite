cbd6943c30e6db52b4e9b2e634cd00fa9a45900f
Karol Ulanowski <ulanowskikarol@gmail.com>
2021-05-11 16:12:37 +0200

    fix casting

--- train.py
+++ train.py
@@ -23,6 +23,7 @@ from wandb.keras import WandbCallback
 
 @tf.function
 def mse(y_actual, y_pred):
+    y_pred = tf.cast(y_pred, dtype=tf.float32)
     valid = tf.cast(tf.math.reduce_max(y_actual, axis=(1, 2)) > 0, dtype=tf.float32)
     valid_mask = tf.reshape(valid, [tf.shape(y_actual)[0], 1, 1, tf.shape(valid)[-1]])
     return tf.reduce_mean(tf.square(y_actual - y_pred) * valid_mask)
